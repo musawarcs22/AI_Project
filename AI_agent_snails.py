a2442e2e5964df793865777b6e31ef724851761f
manzoor-ul-haq <manzoor.haq00@gmail.com>
2020-11-25 17:32:18 +0500

    Updated bot_move

--- AI_agent_snails.py
+++ AI_agent_snails.py
@@ -63,42 +63,35 @@ class Game(arcade.View):
         		print(board[i][j], end="\t")
         print()
 
-    def evaluateBoard(self):
-        if self.bot_score == 50 and self.human_score == 50:
-            self.game_state = "Draw"
-           # return 5 # for Draw State
-        elif self.bot_score > 50:
-            self.game_state = "BotWon"
-            #return 10 # for Bot Win
-        elif self.human_score > 50:
-            self.game_state = "HumanWon"
-            #return 1 # for Human Win
-        else:
-            for i in range(10):
-                for j in range(10):
-                    if board[i][j] == 0:
-                        self.state = 0  # Continue State
-            #            return 0          game_state = "GameOn" is Continue State
-
-    def evaluateBoard_AI(self):
+    def evaluateBoard_AI(self, board):
         H_count = 0
         B_count = 0
-        for i in range(ROWS):
-            for j in range(COLUMNS):
+
+        for i in range(10):
+            for j in range(10):
                 if board[i][j] == 10:
                     H_count += 1
-        for i in range(ROWS):
-            for j in range(COLUMNS):
+        
+        for i in range(10):
+            for j in range(10):
                 if board[i][j] == 10:
                     B_count += 1
-        if H_count > 50:
-            return 1
+        
         if B_count > 50:
+            self.game_state = "BotWon"
+            return 1
+        elif H_count > 50:
+            self.game_state = "HumanWon"
             return -1
-        if H_count == 50 and B_count == 50:
+        elif H_count == 50 and B_count == 50:
+            self.game_state = "Draw"
             return 0
+        else:
+            if self.isMoveLeft(board):
+                self.state = 0
+                return None
 
-    def isMoveLeft(self):
+    def isMoveLeft(self, board):
         for i in range(ROWS):
             for j in range(COLUMNS):
 
@@ -106,10 +99,56 @@ class Game(arcade.View):
                     return True
         return False
 
-    def minimax(self, depth, maxlevel):
-        pass
+    def minimax(self, board, depth, isAgentTurn):
+        win = 1
+        draw = 0
+        lose = -1
 
-    def heuristic(self):
+        result = self.evaluateBoard_AI(board)
+
+        if(result == win or result == draw or result == lose):
+            
+            bestScore = result
+            bestChild = board
+
+
+        elif isAgentTurn:
+            
+            bestScore = -1000
+
+            if result == None:
+                score = self.heuristic(board)
+
+            for i in range(ROWS):
+                for j in range(COLUMNS):
+
+                    if board[i][j] == 0:
+                        board[i][j] = self.bot
+                        
+                        score = minimax(board, depth+1, False)
+                        
+                        bestScore = max(score, bestScore)
+                        bestChild = {j, i}
+            
+            return bestScore
+
+        else:
+
+            bestScore = 1000
+            
+            for i in range(ROWS):
+                for j in range(COLUMNS):
+                    
+                    if board[i][j] == 0:
+                        board[i][j] = self.human
+                        score = minimax(board, depth+1, True)
+                        
+                        bestScore = min(score, bestScore)
+                        bestChild = {j, i}
+            
+            return bestScore
+
+    def heuristic(self, board):
         
         x, y, visitedBoxes, rightBoxes, leftBoxes, topBoxes, bottomBoxes, winningChances = 0, 0, 0, 0, 0, 0, 0, 0
         
@@ -118,15 +157,13 @@ class Game(arcade.View):
             for j in range(COLUMNS):
                 if board[i][j] == self.bot_Splash:
                     visitedBoxes += 1
-                else:
-                    continue
         winningChances += visitedBoxes
         
         #giving index of bot position
         for i in range(ROWS):
             for j in range(COLUMNS):
                 if board[i][j] == self.bot:
-                    x, y = i, j
+                    x, y = j, i
                     break
         
         #below loops are counting zero boxes on all four sides of the bot
@@ -155,7 +192,7 @@ class Game(arcade.View):
         winningChances += max(rightBoxes, leftBoxes, topBoxes, bottomBoxes)
         
         # 10 will be added to the variable ‘winnigChances’ if bot is in central area.
-        if((x != 0 and x != 9) and (y != 9 and y != 9)):
+        if((x != 0 and x != 9) and (y != 0 and y != 9)):
             winningChances += 10
         
         return winningChances
@@ -173,11 +210,79 @@ class Game(arcade.View):
         hx = self.human_Location[0]
         hy = self.human_Location[1]
         #Desired Location where the mouse is clicked 
+        left_winingChance, right_winingChance, top_winingChance, bottom_winingChance = 0, 0, 0, 0
+        left, right, top, bottom = -1, -1, -1, -1
+
+        try:
+            left = board[bx-1][by]
+        except:
+            pass
+        try:
+            right = board[bx+1][by]
+        except:
+            pass
+        try:
+            top = board[bx][by+1]
+        except:
+            pass
+        try:
+            bottom = board[bx][by-1]
+        except:
+            pass
+
+        if left == 0:
+            board[bx][by] = 20
+            board[bx-1][by] = 2
+            left_winingChance = self.heuristic(board)
+            board[bx][by] = 2
+            board[bx-1][by] = 0
+
+        if right == 0:
+            board[bx][by] = 20
+            board[bx+1][by] = 2
+            right_winingChance = self.heuristic(board)
+            board[bx][by] = 2
+            board[bx+1][by] = 0
+
+        if top == 0:
+            board[bx][by] = 20
+            board[bx][by+1] = 2
+            top_winingChance = self.heuristic(board)
+            board[bx][by] = 2
+            board[bx][by+1] = 0
+
+        if bottom == 0:
+            board[bx][by] = 20
+            board[bx][by-1] = 2
+            bottom_winingChance = self.heuristic(board)
+            board[bx][by] = 2
+            board[bx][by-1] = 0
         
+        if left_winingChance == max(left_winingChance, right_winingChance, top_winingChance, bottom_winingChance):
+            board[bx][by] = 20
+            board[bx-1][by] = 2
+            self.Bot_Location[0] = bx-1
+            self.Bot_Location[1] = by
 
+        elif right_winingChance == max(left_winingChance, right_winingChance, top_winingChance, bottom_winingChance):
+            board[bx][by] = 20
+            board[bx+1][by] = 2
+            self.Bot_Location[0] = bx+1
+            self.Bot_Location[1] = by
+        
+        elif top_winingChance == max(left_winingChance, right_winingChance, top_winingChance, bottom_winingChance):
+            board[bx][by] = 20
+            board[bx][by+1] = 2
+            self.Bot_Location[0] = bx
+            self.Bot_Location[1] = by+1
 
-
-        if self.isMoveLeft():
+        elif bottom_winingChance == max(left_winingChance, right_winingChance, top_winingChance, bottom_winingChance):
+            board[bx][by] = 20
+            board[bx][by-1] = 2
+            self.Bot_Location[0] = bx
+            self.Bot_Location[1] = by-1
+        
+        if self.isMoveLeft(board):
             pass
 
     def on_key_press(self, key, modifiers):
@@ -191,12 +296,16 @@ class Game(arcade.View):
             box = [x//G_SIZE, y//G_SIZE]    #Location of box(x,y)
             if(self.is_Legal_Move(box)):
                 self.update_grid(box)
-                self.evaluateBoard()
+                self.evaluateBoard_AI(board)
                 self.bot_move()
+                self.update_grid(box)
+                self.evaluateBoard_AI(board)
             else: #This else will execute in case the move is illegal. 
                 if self.turn == 1000:
                     self.human_score -= 1
+                    self.turn = 2000
                     self.bot_move()
+                    self.update_grid(box)
 
                 
             
@@ -210,7 +319,7 @@ class Game(arcade.View):
         if x <= game_SCREEN_WIDTH and y <= game_SCREEN_HEIGHT: #Checking if clicked out side screen
             
             #The following long if is checking moving on the opponent's slime
-            if (self.turn == 1000 and (board[x][y] == 0 or board[x][y] == 10)) or ((self.turn == 2000) and (board[x][y] == 0 or board[x][y] == 20)): 
+            if self.turn == 1000 and (board[x][y] == 0 or board[x][y] == 10): 
                
                 #--------Checking Snail is not bypassing a square Start--------------#
                 try: # Using these try and except blocks to ignore index error 
@@ -261,39 +370,7 @@ class Game(arcade.View):
                         if (left == 1 or right == 1 or top == 1 or bottom == 1 ):
                             return True
                         return False
-                
-                elif self.turn == 2000:
-                    if x == 0 and y == 9:
-                        if  (right == 2 or bottom == 2):
-                            return True
-                        return False
-                    elif x == 9 and y == 0:
-                        if (top == 2 or left == 2):
-                            return True
-                        return False
-                    elif x == 9 or x == 0 or y == 0 or y == 9:
-                        if x == 0:
-                            if (top == 2 or bottom == 2 or right == 2 ):
-                                return True
-                            return False
-                        elif x == 9:
-                            if (top == 2 or bottom == 2 or left == 2 ):
-                                return True
-                            return False
-                        elif y == 0:
-                            if (left == 2 or top == 2 or right == 2):
-                                return True
-                            return False
-                        elif y == 9:
-                            if (left == 2 or bottom == 2 or right == 2):
-                                return True
-                            return False
-                        else:
-                            return False
-                    elif (x > 0 or x < 9) and (y > 0 or y < 9):
-                        if  (left == 2 or right == 2 or top == 2 or bottom == 2 ):
-                            return True
-                        return False
+
                     #--------Checking Snail is not bypassing a square End--------------#
             else:
                 return False
@@ -310,7 +387,7 @@ class Game(arcade.View):
         """
 
         #Present Location of Bot 
-        bx = self.Bot_Location[0]
+        bx = self.Bot_Location[0] 
         by = self.Bot_Location[1]
         #Present Location of Human 
         hx = self.human_Location[0]
@@ -319,12 +396,12 @@ class Game(arcade.View):
         cx = box[0]
         cy = box[1]
         #----------------------------------------------------
-        if self.turn == 1000:   #-------> HUMAN's TURN <------------
+        if self.turn == 1000:     #-------> HUMAN's TURN <------------
             board[hx][hy] = 10    #Placing Splash on the Present human Location 
             self.human_score += 1 #Increasing Human Score     
             self.turn = 2000      #Changing Turn
             if board[cx][cy] == 10: #This will execute when the Human clicks on his Splash(Slippery Functionality for Human)
-                
+                self.human_score -= 1 #No Score should be increased if clicked on splash 
                 if hx == cx and cy > hy:
                     for y in range(hy+1, 10, 1):
                         if y == 9 and board[cx][y]==10:
@@ -371,7 +448,7 @@ class Game(arcade.View):
                         self.human_Location = box
                         return
                     for x in range(cx-1, -1, -1):
-                        if x == 0:
+                        if x == 0 and board[x][cy] == 10:
                             board[x][cy] = 1
                             self.human_Location[0] = x
                             self.human_Location[1] = cy
@@ -382,7 +459,7 @@ class Game(arcade.View):
                             self.human_Location[1] = cy
                             break
                 elif cx == hx and cy == hy: #When Player will click on itself it will lose the turn and scores will remain the same
-                    self.human_score -= 1
+                    self.human_score -= 1 
                 #----------------------------------------------------------------------------
                 
             else:                   #This will execute When Human clicks Empty Square
@@ -392,82 +469,15 @@ class Game(arcade.View):
                 #----------------------------------------------------------------------------
                 #----------------------------------------------------------------------------
         elif self.turn == 2000:      #-----> BOT's TURN <-----
-            board[bx][by] = 20       #Putting Splash on the Bot Location
+            # board[bx][by] = 20       #Putting Splash on the Bot Location
             self.bot_score += 1      #Increasing Bot Score 
             self.turn = 1000         #Fliping turn       
-            if board[cx][cy] == 20:  #This will execute when the Bot clicks on his Splash(Slippery Functionality for Bot)   
-                if bx == cx and cy > by:
-                    for y in range(by+1, 10, 1):
-                        if y == 9 and board[cx][y] == 20:
-                            board[cx][y] = 2
-                            self.Bot_Location[0] = cx
-                            self.Bot_Location[1] = y
-                            break
-                        elif board[cx][y] == 0 or board[cx][y] == 10 or board[cx][y] == 1:
-                            board[cx][y-1] = 2
-                            self.Bot_Location[0] = cx
-                            self.Bot_Location[1] = y-1
-                            break
-                elif bx == cx and cy <  by:
-                    for y in range(by-1, -1, -1):
-                        if y == 0 and board[cx][y] == 20:
-                            board[cx][y] = 2
-                            self.Bot_Location[0] = cx
-                            self.Bot_Location[1] = y
-                            break
-                        elif board[cx][y] == 0 or board[cx][y] == 10 or board[cx][y] == 1:
-                            board[cx][y+1] = 2
-                            self.Bot_Location[0] = cx
-                            self.Bot_Location[1] = y+1
-                            break
-                elif cx > bx and cy == by:
-                    if cx == 9:              #Right Side Corner case
-                        board[cx][cy] = 2
-                        self.Bot_Location = box
-                        
-                        return
-                    for x in range(cx+1, 10, 1):
-                        if x == 9:
-                            print("Chaladadasdw")
-                            board[x][cy] = 2
-                            self.Bot_Location[0] = x
-                            self.Bot_Location[1] = cy
-                            break
-                        elif board[x][cy] == 0 or board[x][cy] == 10 or board[x][cy] == 1:
-                            board[x-1][cy] = 2
-                            self.Bot_Location[0] = x-1
-                            self.Bot_Location[1] = cy
-                            break
-                elif cx < bx and cy == by:
-                    if cx == 0:              #Left Side Corner case
-                        board[cx][cy] = 2
-                        self.Bot_Location = box
-                        return
-                    for x in range(cx-1, -1, -1):
-                        if x == 0:
-                            board[x][cy] = 2
-                            self.Bot_Location[0] = x
-                            self.Bot_Location[1] = cy
-                            break
-                        elif board[x][cy] == 0 or board[x][cy] == 10 or board[x][cy] == 1:
-                            board[x+1][cy] = 2
-                            self.Bot_Location[0] = x+1
-                            self.Bot_Location[1] = cy
-                            break
-                elif cx == bx and cy == by:
-                    self.bot_score -= 1 
-                #---------------------------------------------   
-            else:    #This will execute When Bot clicks Empty Square
-                board[bx][by] = 20
-                board[cx][cy] = 2
-                self.Bot_Location = box
-                #---------------------------------------------
 
         print("-----------------------------------------------")
         for j in range(ROWS):
-        	print()
-        	for i in range(COLUMNS):
-        		print(board[i][j], end="\t")
+            print()
+            for i in range(COLUMNS):
+                print(board[i][j], end="\t")
         print()
         
     def on_show(self):
@@ -475,8 +485,6 @@ class Game(arcade.View):
         
     def on_draw(self):
         arcade.start_render()
-        # self.shape_list = arcade.ShapeElementList()
-        # self.shape_list.draw()
 
         if self.game_state == "GameMenu":
             arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, menuEmoji)
@@ -487,8 +495,6 @@ class Game(arcade.View):
             
 
         elif self.game_state == "GameOn":
-            
-            
 
             # setting the background image
             arcade.draw_lrwh_rectangle_textured(0, 0, game_SCREEN_WIDTH, game_SCREEN_HEIGHT, background)
@@ -560,7 +566,6 @@ def main():
 
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT,SCREEN_TITLE)
     game_view = Game()
-    game_view.heuristic()
     window.show_view(game_view)
     arcade.run()
     
